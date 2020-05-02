0fb702ac581702bedfdcbee1a14627911145a08a
Sergeev <SergeevSA1978@ya.ru>
2020-05-02 19:32:59 +0300

    New: java.io.File Used earlier: Comparable<T>, Comparator<T>, TreeSet<E>, try-catch-finally, throw, throws, интерфейсы, дефолтные методы, абстрактные классы, множественное наследование, переопределение методов, StringBuffer, статические поля, статические методы, BufferedReader, InputStreamReader, parseInt, ArrayList<>, IOException, StringBuilder, массивы, equals, Scanner, Arrays.copyOf, Arrays.fill, this, циклы(for, while), Math.random, final.

---
+++ src/com/startjavanew/Game2/InputData/ImportFile.java
@@ -0,0 +1,14 @@
+package com.startjavanew.Game2.InputData;
+
+import java.io.File;
+
+public class ImportFile {
+    public static void main(String[] args) {
+        File src = new File("D:/");
+        File [] files = src.listFiles();
+
+        for(File file : files) {
+            System.out.println(file.getName());
+        }
+    }
+}
